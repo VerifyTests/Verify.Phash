f06de18f77fbd2926ad78857c796d2826fbee09c
SimonCropp <simon.cropp@gmail.com>
2020-06-07 11:51:24 +1000

    move to nunit

--- src/Tests/Samples.cs
+++ src/Tests/Samples.cs
@@ -1,52 +1,45 @@
 ﻿using System.IO;
 using System.Threading.Tasks;
 using Verify;
-using VerifyXunit;
-using Xunit;
-using Xunit.Abstractions;
+using VerifyNUnit;
+using NUnit.Framework;
 
 #region TestDefinition
-public class Samples :
-    VerifyBase
+public class Samples
 {
-    public Samples(ITestOutputHelper output) :
-        base(output)
-    {
-    }
-
     static Samples()
     {
         VerifyPhash.Initialize();
     }
     #endregion
 
-    [Fact]
+    [Test]
     public async Task Simple()
     {
         var settings = new VerifySettings();
         settings.UseExtension("png");
-        await Verify(File.OpenRead("sample.png"), settings);
-        await Verify(File.OpenRead("sample2.png"), settings);
+        await Verifier.Verify(File.OpenRead("sample.png"), settings);
+        await Verifier.Verify(File.OpenRead("sample2.png"), settings);
     }
 
     #region VerifyPng
 
-    [Fact]
+    [Test]
     public Task VerifyPng()
     {
-        return VerifyFile("sample.png");
+        return Verifier.VerifyFile("sample.png");
     }
 
     #endregion
 
     #region VerifyPngStream
 
-    [Fact]
+    [Test]
     public Task VerifyPdfStream()
     {
         var settings = new VerifySettings();
         settings.UseExtension("png");
-        return Verify(File.OpenRead("sample.png"), settings);
+        return Verifier.Verify(File.OpenRead("sample.png"), settings);
     }
 
     #endregion
