a5c64b6606799d493d11de64521d10051007df88
Simon Cropp <simon.cropp@gmail.com>
2021-10-10 10:54:46 +1100

    cleanup

--- src/Verify.Phash/PhashSettings.cs
+++ src/Verify.Phash/PhashSettings.cs
@@ -1,60 +1,55 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 
-namespace VerifyTests
+namespace VerifyTests;
+
+public static class PhashSettings
 {
-    public static class PhashSettings
+    public static void PhashCompareSettings(
+        this VerifySettings settings,
+        float threshold = 0.999f,
+        float sigma = 3.5f,
+        float gamma = 1f,
+        int angles = 180)
     {
-        public static void PhashCompareSettings(
-            this VerifySettings settings,
-            float threshold = 0.999f,
-            float sigma = 3.5f,
-            float gamma = 1f,
-            int angles = 180)
+        PhashCompareSettings(threshold, sigma, gamma, angles, settings.Context);
+    }
+
+    public static SettingsTask PhashCompareSettings(
+        this SettingsTask settings,
+        float threshold = 0.999f,
+        float sigma = 3.5f,
+        float gamma = 1f,
+        int angles = 180)
+    {
+        PhashCompareSettings(threshold, sigma, gamma, angles, settings.CurrentSettings.Context);
+        return settings;
+    }
+
+    static void PhashCompareSettings(
+        float threshold,
+        float sigma,
+        float gamma,
+        int angles,
+        Dictionary<string, object> context)
+    {
+        context["Phash"] = new PhashCompareSettings(
+            threshold,
+            sigma,
+            gamma,
+            angles);
+    }
+
+    internal static bool GetPhashCompareSettings(
+        this IReadOnlyDictionary<string, object> context,
+        [NotNullWhen(true)] out PhashCompareSettings? pagesSettings)
+    {
+        if (context.TryGetValue("Phash", out var value))
         {
-            Guard.AgainstNull(settings, nameof(settings));
-            PhashCompareSettings(threshold, sigma, gamma, angles, settings.Context);
+            pagesSettings = (PhashCompareSettings) value;
+            return true;
         }
 
-        public static SettingsTask PhashCompareSettings(
-            this SettingsTask settings,
-            float threshold = 0.999f,
-            float sigma = 3.5f,
-            float gamma = 1f,
-            int angles = 180)
-        {
-            Guard.AgainstNull(settings, nameof(settings));
-            PhashCompareSettings(threshold, sigma, gamma, angles, settings.CurrentSettings.Context);
-            return settings;
-        }
-
-        static void PhashCompareSettings(
-            float threshold,
-            float sigma,
-            float gamma,
-            int angles,
-            Dictionary<string, object> context)
-        {
-            context["Phash"] = new PhashCompareSettings(
-                threshold,
-                sigma,
-                gamma,
-                angles);
-        }
-
-        internal static bool GetPhashCompareSettings(
-            this IReadOnlyDictionary<string, object> context,
-            [NotNullWhen(true)] out PhashCompareSettings? pagesSettings)
-        {
-            Guard.AgainstNull(context, nameof(context));
-            if (context.TryGetValue("Phash", out var value))
-            {
-                pagesSettings = (PhashCompareSettings) value;
-                return true;
-            }
-
-            pagesSettings = null;
-            return false;
-        }
+        pagesSettings = null;
+        return false;
     }
 }
--- src/Verify.Phash/VerifyPhash.cs
+++ src/Verify.Phash/VerifyPhash.cs
@@ -1,66 +1,62 @@
-﻿using System.Collections.Generic;
-using System.Drawing;
-using System.IO;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using Shipwreck.Phash;
 using Shipwreck.Phash.Bitmaps;
 
-namespace VerifyTests
+namespace VerifyTests;
+
+public static class VerifyPhash
 {
-    public static class VerifyPhash
+    public static void Initialize()
     {
-        public static void Initialize()
+        RegisterComparer("png");
+    }
+
+    public static void RegisterComparer(
+        string extension,
+        float threshold = 0.999f,
+        float sigma = 3.5f,
+        float gamma = 1f,
+        int angles = 180)
+    {
+        Guard.AgainstNullOrEmpty(extension, nameof(extension));
+        VerifierSettings.RegisterStreamComparer(
+            extension,
+            (received, verified, context) => Compare(context, received, verified, threshold, sigma, gamma, angles));
+    }
+
+    static Task<CompareResult> Compare(
+        IReadOnlyDictionary<string, object> context,
+        Stream received,
+        Stream verified,
+        float threshold,
+        float sigma,
+        float gamma,
+        int angles)
+    {
+        if (context.GetPhashCompareSettings(out var innerSettings))
         {
-            RegisterComparer("png");
+            threshold = innerSettings.Threshold;
+            sigma = innerSettings.Sigma;
+            gamma = innerSettings.Gamma;
+            angles = innerSettings.Angles;
         }
 
-        public static void RegisterComparer(
-            string extension,
-            float threshold = 0.999f,
-            float sigma = 3.5f,
-            float gamma = 1f,
-            int angles = 180)
+        var hash1 = HashImage(received, sigma, gamma, angles);
+        var hash2 = HashImage(verified, sigma, gamma, angles);
+        var score = ImagePhash.GetCrossCorrelation(hash1, hash2);
+        var compare = score > threshold;
+        if (compare)
         {
-            Guard.AgainstNullOrEmpty(extension, nameof(extension));
-            VerifierSettings.RegisterStreamComparer(
-                extension,
-                (received, verified, context) => Compare(context, received, verified, threshold, sigma, gamma, angles));
+            return Task.FromResult(CompareResult.Equal);
         }
 
-        static Task<CompareResult> Compare(
-            IReadOnlyDictionary<string, object> context,
-            Stream received,
-            Stream verified,
-            float threshold,
-            float sigma,
-            float gamma,
-            int angles)
-        {
-            if (context.GetPhashCompareSettings(out var innerSettings))
-            {
-                threshold = innerSettings.Threshold;
-                sigma = innerSettings.Sigma;
-                gamma = innerSettings.Gamma;
-                angles = innerSettings.Angles;
-            }
+        return Task.FromResult(CompareResult.NotEqual($"diff > threshold. threshold: {threshold}, score: {score}"));
+    }
 
-            var hash1 = HashImage(received, sigma, gamma, angles);
-            var hash2 = HashImage(verified, sigma, gamma, angles);
-            var score = ImagePhash.GetCrossCorrelation(hash1, hash2);
-            var compare = score > threshold;
-            if (compare)
-            {
-                return Task.FromResult(CompareResult.Equal);
-            }
-
-            return Task.FromResult(CompareResult.NotEqual($"diff > threshold. threshold: {threshold}, score: {score}"));
-        }
-
-        static Digest HashImage(Stream stream, float sigma, float gamma, int angles)
-        {
-            using var bitmap = (Bitmap) Image.FromStream(stream);
-            var image = bitmap.ToLuminanceImage();
-            return ImagePhash.ComputeDigest(image, sigma, gamma, angles);
-        }
+    static Digest HashImage(Stream stream, float sigma, float gamma, int angles)
+    {
+        using var bitmap = (Bitmap) Image.FromStream(stream);
+        var image = bitmap.ToLuminanceImage();
+        return ImagePhash.ComputeDigest(image, sigma, gamma, angles);
     }
 }
